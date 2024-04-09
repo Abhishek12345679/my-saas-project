27fb19c4cb67dee40c6b497d6a6f817cf2cbcf14
Abhishek Sah <sah755146@gmail.com>
2024-04-10 01:02:19 +0530

    Refactor font loading and update project name in og/route.tsx

--- components/sections/hero-landing.tsx
+++ components/sections/hero-landing.tsx
@@ -42,7 +42,7 @@ export async function HeroLanding() {
         <h1 className="text-balance font-urban text-4xl font-extrabold tracking-tight sm:text-5xl md:text-6xl lg:text-[66px]">
           Kick off with a bang with{" "}
           <span className="text-gradient_indigo-purple font-extrabold">
-            SaaS Starter
+            Upcode
           </span>
         </h1>
 
@@ -50,8 +50,8 @@ export async function HeroLanding() {
           className="max-w-2xl text-balance leading-normal text-muted-foreground sm:text-xl sm:leading-8"
           style={{ animationDelay: "0.35s", animationFillMode: "forwards" }}
         >
-          Build your next project using Next.js 14, Prisma, Neon, Auth.js
-          v5, Resend, React Email, Shadcn/ui, Stripe.
+          Build your next project using Next.js 14, Prisma, Neon, Auth.js v5,
+          Resend, React Email, Shadcn/ui, Stripe.
         </p>
 
         <div
